a3e5f109793619ee87af4181a4f44ba562f83ad0
Mohammed Ali-A <god55633@gmail.com>
2023-05-03 12:59:55 -0400

    made it better.

--- a calculator i guess.py
+++ a calculator i guess.py
@@ -3,12 +3,14 @@ num1 = int(input("What's the first number? "))
 num2 = int(input("What's the second number? "))
 
 if operation == "addition":
-    print(num1 + num2)
+    ans = (num1 + num2)
 elif operation == "subtraction":
-    print(num1 - num2)
+    ans = (num1 - num2)
 elif operation == "multiplication":
-    print(num1 * num2)
+    ans = (num1 * num2)
 elif operation == "division":
-    print(num1 / num2)
+    ans = (num1 / num2)
 else:
     print("I don't know that operation type. Try again with no captials, no spaces, and the full name of the operation.")
+
+print("The answer is " + str(ans) + ".")
